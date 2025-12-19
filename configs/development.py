6d4d4ed32dc520662cabcfcbf0a228fb7684824a
Canibalseal <franklyn.eyes1@gmail.com>
2025-12-19 14:02:54 +0100

    feat: update development configuration for SQLAlchemy and define database URI

--- configs/development.py
+++ configs/development.py
@@ -2,4 +2,8 @@ DEBUG = True
 
 # Minimal development config: define only these two variables.
 # DATABASE is a path relative to the project working directory.
-DATABASE = 'app.db'
+## DATABASE = 'app.db' ## Uncomment to use a different path
+
+# SQLAlchemy configuration
+SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
