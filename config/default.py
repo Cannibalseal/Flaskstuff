49e4cce7477086b9e45b7bc8d4d7837a543075e3
Canibalseal <franklyn.eyes1@gmail.com>
2025-12-21 14:50:36 +0100

    Clean up all Celery/Redis references - remove config, old task definitions

--- config/default.py
+++ config/default.py
@@ -37,14 +37,5 @@ class Config:
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '')
 	MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'noreply@yourblog.com')
 
-	# Celery configuration
-	CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-	CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
-	CELERY_TASK_SERIALIZER = 'json'
-	CELERY_RESULT_SERIALIZER = 'json'
-	CELERY_ACCEPT_CONTENT = ['json']
-	CELERY_TIMEZONE = 'UTC'
-	CELERY_ENABLE_UTC = True
-
 
 default_config = Config()
